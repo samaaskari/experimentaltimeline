d27fece7283c801892450a99ffe776ce12e687e2
Sama <11033639+samaaskari@users.noreply.github.com>
2021-04-10 11:23:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
-$(function(){
-  function sumSection(){
-    return $(".container").height()
+$(function () {
+  function sumSection() {
+    return $(".container").height();
   }
-  function setDimensionBar(){
+  function setDimensionBar() {
     $(".bar").css({
-      "height": ($(window).height()/sumSection())*100 + "%"
-    })
+      "height": $(window).height() / sumSection() * 100 + "%" });
+
   }
-  function setSection(){
-        $.each($("section"), function(i, element){
+  function setSection() {
+    $.each($("section"), function (i, element) {
       $(element).css({
-        "min-height": $(window).height()
-      })
-      })
+        "min-height": $(window).height() });
+
+    });
   }
-  
-  function addBehaviours(){
-    let sections = $("section")
-    $.each($(".node"), function(i, element){
-      $(element).on("click", function(e){
-        e.preventDefault()
-        let scroll = $(sections[i]).offset().top
+
+  function addBehaviours() {
+    let sections = $("section");
+    $.each($(".node"), function (i, element) {
+      $(element).on("click", function (e) {
+        e.preventDefault();
+        let scroll = $(sections[i]).offset().top;
         $('html, body').animate({
-          scrollTop: scroll
-        }, 500);
-      })
-    })
+          scrollTop: scroll },
+        500);
+      });
+    });
   }
-  
-  function arrangeNodes(){
-    $(".node").remove()
-    $.each($("section"), function(i, element){
-      let name = $(element).data("name")
-      let node = $("<li class='node'><span>"+name+"</span></li>")
-      $(".timeline").append(node)
+
+  function arrangeNodes() {
+    $(".node").remove();
+    $.each($("section"), function (i, element) {
+      let name = $(element).data("name");
+      let node = $("<li class='node'><span>" + name + "</span></li>");
+      $(".timeline").append(node);
 
       $(node).css({
-        "top": ($(".timeline").height()/$(document).height()) * $(element).offset().top
-      })
-    })
-    addBehaviours()
+        "top": $(".timeline").height() / $(document).height() * $(element).offset().top });
+
+    });
+    addBehaviours();
   }
-  
-  $(window).on("scroll", function(){
-    let top = (window.scrollY/sumSection())*100
+
+  $(window).on("scroll", function () {
+    let top = window.scrollY / sumSection() * 100;
     $(".bar").css({
-        "top": top + "%"
-      })
-    
-  })
-  
-  $(window).on("resize", function(){
-    setSection()
-    arrangeNodes()
-    setDimensionBar()
-  })
-  
+      "top": top + "%" });
+
+
+  });
+
+  $(window).on("resize", function () {
+    setSection();
+    arrangeNodes();
+    setDimensionBar();
+  });
+
   setTimeout(
-    function(){
-      setSection()
-      arrangeNodes()
-      setDimensionBar()      
-    },
-    200
-  )
-})
+  function () {
+    setSection();
+    arrangeNodes();
+    setDimensionBar();
+  },
+  200);
+
+});
